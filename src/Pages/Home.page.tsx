ef272aa6ac5d54c03635cd81bbbc962a73281a16
Edwin Ajong <edwinajong@gmail.com>
2022-10-15 19:33:00 -0500

    included hero section

--- src/Pages/Home.page.tsx
+++ src/Pages/Home.page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero.component";
 import Navbar from "../components/Navbar.component";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
